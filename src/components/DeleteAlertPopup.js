ed45249ec3f2a82d45f8097b9a4a6a82da912a94
unknown <phoebs_ong@dlsu.edu.ph>
2023-04-10 23:35:30 +0800

    app(feature): delete popup

---
+++ src/components/DeleteAlertPopup.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Image, Text, View, Modal, TouchableOpacity, TouchableWithoutFeedback } from "react-native";
+import useIcon from "../hooks/useIcon";
+import { Overlay } from "react-native-elements";
+import modalStyles from "./utils/modalStyles";
+import popupStyles from "./utils/popupStyles";
+
+const DeleteAlertPopup = ({is2ndModalVisible, set2ndModalVisible, set1stModalVisible, deleteWhatText}) => {
+    const deleteAlertIcon = useIcon("deleteAlertIcon");
+
+    const isModalVisible = is2ndModalVisible;
+    const setModalVisible = set2ndModalVisible
+
+    return (
+        <View style={popupStyles.popupContainer}>
+            <Overlay 
+                isVisible={isModalVisible} 
+                onBackdropPress={setModalVisible} 
+                children={Modal} 
+                overlayStyle={popupStyles.popupContentContainer}
+                animationType="slide"
+                backdropStyle={{backgroundColor: "rgba(0, 0, 0, 0.6)"}}
+            >
+                <View style={popupStyles.popupDeleteDescriptionContainer}>
+                    <Image
+                        source={deleteAlertIcon}
+                        style={popupStyles.popupDeleteAlertIcon}
+                    />
+                    
+                    <Text style={popupStyles.popupDeleteDescriptionText}>Are you sure you want to {deleteWhatText}?</Text>
+                </View>
+
+                <View style={popupStyles.popupCancelDeleteButtonContainer}>
+                    <TouchableOpacity 
+                        onPress={()=> set1stModalVisible(false)}
+                        style={popupStyles.popupButtonContainer}
+                    >
+                        <View>
+                            <Text style={popupStyles.popupCancelText}>CANCEL</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity 
+                        onPress={()=> set1stModalVisible(false)}
+                        style={popupStyles.popupButtonContainer}
+                    >
+                        <View>
+                            <Text style={popupStyles.popupDeleteText}>DELETE</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            
+            </Overlay>
+
+        </View>
+
+    );
+};
+
+export default DeleteAlertPopup;
