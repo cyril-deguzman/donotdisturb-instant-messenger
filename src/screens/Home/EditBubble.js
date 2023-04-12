2bb74c330406ef218c82ad8bcd3d4f9f2ee248ac
cyril-deguzman <cyrilethan.deguzman@gmail.com>
2023-04-12 14:35:05 +0800

    app(screen) edit bubble screen

--- src/screens/Home/EditBubble.js
+++ src/screens/Home/EditBubble.js
@@ -1,6 +1,6 @@
-import React, {useState} from "react";
-import { View, Text, Image, TouchableOpacity} from "react-native";
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React, { useState } from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 import { ScrollView } from "react-native-gesture-handler";
 
@@ -10,14 +10,14 @@ import useIndicator from "../../hooks/useIndicator";
 import useBackground from "../../hooks/useBackground";
 import useIcon from "../../hooks/useIcon";
 import messagesStyles from "./utils/messagesStyles";
-import statusForSpecificAudienceStyles from "./utils/statusForSpecificAudienceStyles"
+import statusForSpecificAudienceStyles from "./utils/statusForSpecificAudienceStyles";
 import AudienceBox from "../../components/AudienceBox";
 import OptionHeaderBox from "../../components/OptionHeaderBox";
 import settingsStyles from "./utils/settingsStyles";
 import ChatInfoOptionBox from "../../components/ChatInfoOptionBox";
 import ChatInfoStatusBox from "../../components/ChatInfoStatusBox";
 
-const EditBubble = ({navigation}) => {
+const EditBubble = ({ navigation }) => {
   const bgImg = useBackground("topBubbles");
   const indicator = useIndicator("idle");
 
@@ -29,66 +29,71 @@ const EditBubble = ({navigation}) => {
 
   return (
     <SafeAreaView style={statusForSpecificAudienceStyles.container}>
-      <Image source={bgImg} style={messagesStyles.bgImg} />
-      <Header title="Some Group Name" navigation={navigation}/>
+      <ScrollView>
+        <Image source={bgImg} style={messagesStyles.bgImg} />
+        <Header title="Some Group Name" navigation={navigation} />
 
- 
         <View style={statusForSpecificAudienceStyles.bgContainer}>
-          
-          
-            <View style={statusForSpecificAudienceStyles.descriptionContainer}>
-              <View style={statusForSpecificAudienceStyles.descriptionTextContainer}>
-                <Text style={statusForSpecificAudienceStyles.descriptionTextHeader}>Edit Members</Text>
-                <Text style={statusForSpecificAudienceStyles.descriptionTextContent}>
-                    You can edit in this section the name of your Bubble, edit the members within your Bubble and see the members within your Bubble. You are also able to edit how your Bubble sees you.</Text>
-              </View>
+          <View style={statusForSpecificAudienceStyles.descriptionContainer}>
+            <View
+              style={statusForSpecificAudienceStyles.descriptionTextContainer}
+            >
+              <Text
+                style={statusForSpecificAudienceStyles.descriptionTextHeader}
+              >
+                Edit Bubble
+              </Text>
+              <Text
+                style={statusForSpecificAudienceStyles.descriptionTextContent}
+              >
+                You can edit in this section the name of your Bubble, edit the
+                members within your Bubble and see the members within your
+                Bubble. You are also able to edit how your Bubble sees you.
+              </Text>
             </View>
-        
-          
-            <View style={settingsStyles.chatInfoGroupContainer}>
-                <OptionHeaderBox header="Audience Info" />
-                <ChatInfoOptionBox
-                icon={changeGroupNameIcon}
-                name="Change group name"
-                navigation={navigation}
-                routeName={"EditBubble"}
-              />
-              <ChatInfoOptionBox
-                icon={editMembersIcon}
-                name="Edit members"
-                navigation={navigation}
-                routeName={"EditMembers"}
-              />
-              <ChatInfoOptionBox
-                icon={seeMembersIcon}
-                name="See members"
-                navigation={navigation}
-                routeName={"SeeMembers"}
-              />
-              <Text>{'\n'}</Text>
+          </View>
 
-              <OptionHeaderBox header="More Actions" />
-              <ChatInfoStatusBox
-                icon={changeStatusIcon}
-                name="Change how they see you"
-                navigation={navigation}
-                routeName={"EditBubble"}
-                userStatus={"idle"}
-              />
-              <ChatInfoOptionBox
-                icon={deleteIcon}
-                name="Delete audience"
-                navigation={navigation}
-                routeName={"EditBubble"}
-              />
-              
-              
+          <View style={settingsStyles.chatInfoGroupContainer}>
+            <OptionHeaderBox header="Audience Info" />
+            <ChatInfoOptionBox
+              icon={changeGroupNameIcon}
+              name="Change group name"
+              navigation={navigation}
+              routeName={"EditBubble"}
+            />
+            <ChatInfoOptionBox
+              icon={editMembersIcon}
+              name="Edit members"
+              navigation={navigation}
+              routeName={"EditMembers"}
+            />
+            <ChatInfoOptionBox
+              icon={seeMembersIcon}
+              name="See members"
+              navigation={navigation}
+              routeName={"SeeMembers"}
+            />
+            <Text>{"\n"}</Text>
 
-            </View>
+            <OptionHeaderBox header="More Actions" />
+            <ChatInfoStatusBox
+              icon={changeStatusIcon}
+              name="Change how they see you"
+              navigation={navigation}
+              routeName={"EditBubble"}
+              userStatus={"idle"}
+            />
+            <ChatInfoOptionBox
+              icon={deleteIcon}
+              name="Delete audience"
+              navigation={navigation}
+              routeName={"EditBubble"}
+            />
+          </View>
         </View>
-    
+      </ScrollView>
     </SafeAreaView>
-  )
+  );
 };
 
 export default EditBubble;
