ee4b1823e9e6dafa3c7453cce41f4c7ff637a9ae
Rayden Lizan <rayden_e_lizan@dlsu.edu.ph>
2023-04-12 18:02:29 +0800

    Connected screen to db, needs exclude features

--- src/components/AudienceBox.js
+++ src/components/AudienceBox.js
@@ -1,8 +1,21 @@
-import React, { useState }  from "react";
+import React, { useState, useEffect }  from "react";
 import { Image, View, Text, TouchableOpacity, TouchableWithoutFeedback } from "react-native"; 
 
 import useIcon from "../hooks/useIcon";
 import audienceBoxStyles from "./utils/audienceBoxStyles";
+import useIndicator from "../hooks/useIndicator";
+
+//import {firebase} from "../../../config/firebase";
+
+import {
+    collection,
+    query,
+    where,
+    getDoc,
+    onSnapshot,
+    doc,
+  } from "firebase/firestore";
+  import { auth, database } from "../../config/firebase";
 
 
 const AudienceBox = (props) => {
@@ -19,11 +32,131 @@ const AudienceBox = (props) => {
 
     const openCloseIcon = isExcludePersonVisible ? openIcon : closeIcon;
 
+    //console.log(props.members);
+    const [bubbleMembers, setBubbleMembers] = useState([]);
+    const [statusForMessage, setStatusForMessage] = useState("");
+    const [indicator, setIndicator] = useState("");
+
+ 
+
+    useEffect(() => {
+
+        const q = query(
+          collection(database, "bubble_members"),
+        );
+    
+        const unsubscribe = onSnapshot(q, async (querySnapshot) => {
+          const bubbleMembersArray = [];
+          let counter = 0;
+
+          
+          if (querySnapshot.empty) return;
+    
+          querySnapshot.forEach(async (doc) => {
+            counter++;
+            const userRef = doc.data().memberID;
+            const dataSnap = await getDoc(userRef);
+            const bubbleSnap = await getDoc(doc.data().bubbleID);
+            
+            
+
+             console.log("usee");
+            // console.log(dataSnap.data());
+            console.log(bubbleSnap.data().bubbleID);
+            console.log(props.bubbleID);
+
+            if (bubbleSnap.data().bubbleID == props.bubbleID)
+            {
+                bubbleMembersArray.push(dataSnap.data().name,
+                );
+
+            }
+
+
+
+            if (querySnapshot.size == counter)
+            {
+                if(bubbleMembersArray.length == 1)
+                    setStatusForMessage("Status for: " + bubbleMembersArray[0]);
+
+                else if (bubbleMembersArray.length == 2)
+                    setStatusForMessage("Status for: " + bubbleMembersArray[0] + ", " + bubbleMembersArray[1]);
+
+                else
+                    setStatusForMessage("Status for: " + bubbleMembersArray[0] + ", " + bubbleMembersArray[1] + ", and " + (bubbleMembersArray.length-1).toString() + " more");
+
+
+                setBubbleMembers(bubbleMembersArray);
+            }
+            
+            
+   
+        });
+
+            const osiSnap = await getDoc(props.audienceIndicator);
+            console.log("props.audienceIndicator()");
+            console.log(osiSnap.data());
+            if(osiSnap.data().osi == "Open To Chat")
+                setIndicator(useIndicator("openToChat"));
+
+            else if(osiSnap.data().osi == "Be Right Back")
+                setIndicator(useIndicator("idle"));
+
+            else if(osiSnap.data().osi == "Do Not Disturb")
+                setIndicator(useIndicator("doNotDisturb"));
+
+            else if(osiSnap.data().osi == "Invisible")
+                setIndicator(useIndicator("invisible"));
+
+    
+          });
+
+          
+
+
+
+        // const osiSnap = getDoc(props.audienceIndicator);
+        // console.log("osiSnap");
+        // console.log(osiSnap);
+        // if(osiSnap.data().osi == "Open To Chat")
+        //     setIndicator("openToChat");
+
+        // else if(osiSnap.data().osi == "Be Right Back")
+        //     setIndicator("idle");
+
+        // else if(osiSnap.data().osi == "Do Not Disturb")
+        //     setIndicator("doNotDisturb");
+
+        // else if(osiSnap.data().osi == "Invisible")
+        //     setIndicator("invisible");
+
+           
+            
+    
+        return () => unsubscribe();
+      }, []);
+
+
+
+
+      console.log("bubbleMembers");
+      console.log(statusForMessage);
+
+    //   console.log("bubbleMembers");
+    //   console.log(bubbleMembers);
+
+
+// const osiSnap = getDoc(props.audienceIndicator);
+//         console.log("osiSnap");
+//         console.log(osiSnap);
+
+
+      
     return (
         <View style={audienceBoxStyles.audienceBoxContainer}>
             <View>
                 <Image 
-                    source={props.audienceIndicator}
+                    source={indicator}
                     style={audienceBoxStyles.audienceBoxIndicator}
                 />
             </View>
@@ -33,12 +166,16 @@ const AudienceBox = (props) => {
                 ): null}
                 
                 <Text style={audienceBoxStyles.audienceBoxAudienceNameText}>{props.title}</Text>
-
+{/* TO DO: Add exlude functionality */}
                 <TouchableWithoutFeedback onPress={() => setExcludePersonVisible(!isExcludePersonVisible)}>
                     <View>
-                        <Text style={audienceBoxStyles.audienceBoxNameListText}>Status for: Leana Rebong, Rayden Lizan, and 10 more</Text>
+
+                    
+
+                            <Text style={audienceBoxStyles.audienceBoxNameListText}>{statusForMessage}</Text>
+               
                         
-                        {isExcludeContainerVisible ? (
+                        {/* {isExcludeContainerVisible ? (
                             <View style={audienceBoxStyles.audienceBoxExcludeHeaderContainer}>
                                 <Image
                                     source={openCloseIcon}
@@ -48,10 +185,10 @@ const AudienceBox = (props) => {
                                 <Text style={audienceBoxStyles.audienceBoxExcludeHeaderText}>Excluding</Text>
                                 
                             </View>
-                        ) : null}
+                        ) : null} */}
                     </View>
                 </TouchableWithoutFeedback>
-
+{/*
                 {isExcludeContainerVisible ? (
                     <View style={audienceBoxStyles.audienceBoxExcludeContainer}>
                         {isExcludePersonVisible ? (
@@ -80,9 +217,10 @@ const AudienceBox = (props) => {
                         ): null}
                         
                     </View>
-                ): null}
+                ): null} */}
 
-                <Text style={audienceBoxStyles.audienceBoxClearStatusText}>Status will clear after tomorrow 8:30 PM</Text>
+{/* TO DO: ADD STATUS CLEAR INFO  */}
+                {/* <Text style={audienceBoxStyles.audienceBoxClearStatusText}>Status will clear after tomorrow 8:30 PM</Text> */}
             </View>
             <View style={audienceBoxStyles.audienceBoxIconsContainer}>
                 <TouchableOpacity>
--- src/screens/Home/StatusForSpecificAudience.js
+++ src/screens/Home/StatusForSpecificAudience.js
@@ -3,109 +3,83 @@ import { View, Text, Image, TouchableOpacity} from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
 import {firebase} from "../../../config/firebase";
 
+import {
+  collection,
+  query,
+  where,
+  getDoc,
+  onSnapshot,
+  doc,
+} from "firebase/firestore";
+import { auth, database } from "../../../config/firebase";
+
 import { ScrollView } from "react-native-gesture-handler";
 
 import Header from "../../components/Header";
 
-import useIndicator from "../../hooks/useIndicator";
+
 import useBackground from "../../hooks/useBackground";
 import useIcon from "../../hooks/useIcon";
 import messagesStyles from "./utils/messagesStyles";
 import statusForSpecificAudienceStyles from "./utils/statusForSpecificAudienceStyles"
 import AudienceBox from "../../components/AudienceBox";
-import { doc } from "firebase/firestore";
+
+
 
 const StatusForSpecificAudience = ({navigation}) => {
   const bgImg = useBackground("topBubbles");
-  const indicator = useIndicator("idle");
+  
   const todoBubbles = firebase.firestore().collection('bubbles');
   const todoRefBubbleMembers = firebase.firestore().collection('bubble_members');
   const todoUserDefaultStatus = firebase.firestore().collection('users');
   const [bubbles, setBubbles] = useState([]);
-  const [bubbleMembers, setBubbleMembers] = useState([]);
+  
 
   const [defaultStatus, setDefaultStatus] = useState(null);
+  const [users, setUsers] = useState([]);
   
 
   useEffect(() => {
-    const subscriber = todoUserDefaultStatus
-      .doc("1")
-      .onSnapshot(documentSnapshot => {
-        console.log('OSI data: ', documentSnapshot.data());
-        setDefaultStatus(documentSnapshot.data().statusID); 
+
+    const userRef = doc(database, "users", auth.currentUser.uid);
+    const q = query(
+      collection(database, "bubbles"),
+      where("creatorID", "==", userRef),
+    );
+
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const bubblesArray = [];
+      let counter = 0;
+      if (querySnapshot.empty) return;
+
+      
+
+      querySnapshot.forEach(async (doc) => {
+
+        counter++;
+
+        const dataSnap = await getDoc(doc.data().statusID);
+        console.log("dataSnap.data()");
+        console.log(dataSnap.data());
+
+        bubblesArray.push({
+
+          bubbleID: doc.data().bubbleID,
+          title: doc.data().title,
+          indicator: doc.data().statusID,
+        });
+
+        if (querySnapshot.size == counter) setBubbles(bubblesArray);
+
 
       });
+    });
 
-      return subscriber;
-
-
+    return () => unsubscribe();
   }, []);
 
-
-  const userDocRef = doc(firebase.firestore(), "users", "1");
-
-  // useEffect(() => {
-  //   const subscriber = todoRefBubbleMembers.doc(bubbleID).onSnapshot(querySnapshot => {
-  //       console.log('Total bubbleMembers: ', querySnapshot.size);
-  //       setBubbleMembers(
-  //         querySnapshot.docs.map((doc) => (    
-  //           {
-  //           memberID: doc.data().memberID,
-
-  //         }))
-  //       );
-
-  //     });
-
-  //     return subscriber;
-
-  // }, []);
-
-  useEffect(() => {
-    const subscriber = todoBubbles.where('creatorID', '==', userDocRef).onSnapshot(querySnapshot => {
-        console.log('Total bubbles: ', querySnapshot.size);
-
-        setBubbles(
-          querySnapshot.docs.map((doc) => 
-             
-
-            // useEffect(() => {
-            //   const subscriber = todoRefBubbleMembers.where('bubbleID', '==', bubbleDoc).onSnapshot(querySnapshot => {
-            //       console.log('Total bubbleMembers: ', querySnapshot.size);
-            //       setBubbleMembers(
-            //         querySnapshot.docs.map((memberDoc) => (    
-            //           {
-            //           memberID: memberDoc.data().memberID,
-            //         }))
-            //       );
-          
-            //     });
-          
-            //     return subscriber;
-          
-            // }, []);
-            
-
-            (    
-            {
-            bubbleID: doc.data().bubbleID,
-            creatorID: doc.data().creatorID,
-            lastChanged: doc.data().lastChanged.toDate(),
-            statusID: doc.data().statusID,
-            title: doc.data().title,
-            //members: {bubbleMembers},
-            //status info: {}
-          }))
-        );
-
-      });
-
-      return subscriber;
-
-  }, []);
-
-  console.log("bubbles");
   console.log(bubbles);
+
 
   return (
     <SafeAreaView style={statusForSpecificAudienceStyles.container}>
@@ -134,25 +108,27 @@ const StatusForSpecificAudience = ({navigation}) => {
                   </View>
                 </TouchableOpacity>
               </View>
-              
 
               
-              {bubbles.map((item) => {
 
-                if(JSON.stringify(item.statusID) != JSON.stringify(defaultStatus))
 
-                  return (
-                      
-                    <AudienceBox audienceIndicator={indicator} title={item.title}/>
+            {bubbles.map((item) => {
 
-                  );
-              })}
+              if(JSON.stringify(item.statusID) != JSON.stringify(defaultStatus))
+              {
+                return (   
+                  <AudienceBox key={item.title} audienceIndicator={item.indicator} title={item.title} bubbleID={item.bubbleID}/>
+                    );
+                }
+            })}
+              
+            </View>  
 
-            </View>
+           
         </View>
     
     </SafeAreaView>
   )
-};
+}
 
 export default StatusForSpecificAudience;
