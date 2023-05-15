f55af18d07c8578a92a40307fc568030d1d3d285
RaydenLizan <74543690+RaydenLizan@users.noreply.github.com>
2023-05-15 14:27:59 +0800

    app(feature): status for specific audience

--- src/components/ResetPopup.js
+++ src/components/ResetPopup.js
@@ -4,56 +4,60 @@ import useIcon from "../hooks/useIcon";
 import { Overlay } from "react-native-elements";
 import popupStyles from "./utils/popupStyles";
 
-const ResetPopup = ({isResetModalVisible, setResetModalVisible}) => {
-    const resetIcon = useIcon("resetIcon");
+const ResetPopup = ({
+  isResetModalVisible,
+  setResetModalVisible,
+  resetStatusFunction,
+  title,
+}) => {
+  const resetIcon = useIcon("resetIcon");
 
-    const isModalVisible = isResetModalVisible;
-    const setModalVisible = setResetModalVisible;
+  const isModalVisible = isResetModalVisible;
+  const setModalVisible = setResetModalVisible;
 
-    return (
-        <View style={popupStyles.popupContainer}>
-            <Overlay 
-                isVisible={isModalVisible} 
-                onBackdropPress={setModalVisible} 
-                children={Modal} 
-                overlayStyle={popupStyles.popupContentContainer}
-                animationType="slide"
-                backdropStyle={{backgroundColor: "rgba(0, 0, 0, 0.6)"}}
-            >
-                <View style={popupStyles.popupDeleteDescriptionContainer}>
-                    <Image
-                        source={resetIcon}
-                        style={popupStyles.popupDeleteAlertIcon}
-                    />
-                    
-                    <Text style={popupStyles.popupDeleteDescriptionText}>Are you sure you want to reset to your default status?</Text>
-                </View>
+  return (
+    <View style={popupStyles.popupContainer}>
+      <Overlay
+        isVisible={isModalVisible}
+        onBackdropPress={setModalVisible}
+        children={Modal}
+        overlayStyle={popupStyles.popupContentContainer}
+        animationType="slide"
+        backdropStyle={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}
+      >
+        <View style={popupStyles.popupDeleteDescriptionContainer}>
+          <Image source={resetIcon} style={popupStyles.popupDeleteAlertIcon} />
 
-                <View style={popupStyles.popupCancelDeleteButtonContainer}>
-                    <TouchableOpacity 
-                        onPress={()=> setModalVisible(false)}
-                        style={popupStyles.popupButtonContainer}
-                    >
-                        <View>
-                            <Text style={popupStyles.popupCancelText}>CANCEL</Text>
-                        </View>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity 
-                        onPress={()=> setModalVisible(false)}
-                        style={popupStyles.popupButtonContainer}
-                    >
-                        <View>
-                            <Text style={popupStyles.popupDeleteText}>RESET</Text>
-                        </View>
-                    </TouchableOpacity>
-                </View>
-            
-            </Overlay>
-
+          <Text style={popupStyles.popupDeleteDescriptionText}>
+            Are you sure you want to reset "{title}" to your default status?
+          </Text>
         </View>
 
-    );
+        <View style={popupStyles.popupCancelDeleteButtonContainer}>
+          <TouchableOpacity
+            onPress={() => setModalVisible(false)}
+            style={popupStyles.popupButtonContainer}
+          >
+            <View>
+              <Text style={popupStyles.popupCancelText}>CANCEL</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              setModalVisible(false);
+              resetStatusFunction();
+            }}
+            style={popupStyles.popupButtonContainer}
+          >
+            <View>
+              <Text style={popupStyles.popupDeleteText}>RESET</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </Overlay>
+    </View>
+  );
 };
 
 export default ResetPopup;
--- src/screens/Home/StatusForSpecificAudience.js
+++ src/screens/Home/StatusForSpecificAudience.js
@@ -10,6 +10,8 @@ import {
   getDoc,
   onSnapshot,
   doc,
+  updateDoc,
+  serverTimestamp,
 } from "firebase/firestore";
 import { auth, database } from "../../../config/firebase";
 
@@ -22,23 +24,80 @@ import useIcon from "../../hooks/useIcon";
 import messagesStyles from "./utils/messagesStyles";
 import statusForSpecificAudienceStyles from "./utils/statusForSpecificAudienceStyles";
 import AudienceBox from "../../components/AudienceBox";
+import ResetPopup from "../../components/ResetPopup";
+import useFetchBubbleMembers from "../../hooks/useFetchBubbleMembers";
 
 const StatusForSpecificAudience = ({ navigation }) => {
   const bgImg = useBackground("topBubbles");
 
   const [bubbles, setBubbles] = useState([]);
+  const [selectedBubble, setSelectedBubble] = useState(null);
+  const [selectedBubbleTitle, setSelectedBubbleTitle] = useState("");
 
   const [defaultStatus, setDefaultStatus] = useState(null);
 
-  // useLayoutEffect(async () => {
-  //   const userRef = doc(database, "users", auth.currentUser.uid);
+  const [isResetModalVisible, setResetModalVisible] = useState(false);
 
-  //   const docSnap = await getDoc(userRef);
+  const resetStatusFunction = async () => {
+    const bubbleRef = doc(database, "bubbles", selectedBubble);
+    console.log("Bubbldsdsdsd!" + selectedBubble);
+    const data = await getDoc(bubbleRef);
+    const currentTime = serverTimestamp();
+    await updateDoc(bubbleRef, {
+      statusID: defaultStatus,
+      lastChanged: currentTime,
+    })
+      .then(() => {
+        console.log("Bubble status reset!");
+        if (!data.data().computerGenerated) {
+          resetBubbleMemberBubbles(bubbleRef, currentTime);
+        }
+      })
+      .catch((error) => console.error(error));
+  };
 
-  //   if (!docSnap.empty) setDefaultStatus(docSnap.data().statusID);
+  const resetBubbleMemberBubbles = async (bubbleRef, currentTime) => {
+    const q = query(
+      collection(database, "bubble_members"),
+      where("bubbleID", "==", bubbleRef)
+    );
 
-  //   return () => unsubscribe();
-  // }, []);
+    onSnapshot(q, async (querySnapshot) => {
+      let counter = 0;
+      console.log("inside snap");
+      if (querySnapshot.empty) return;
+
+      querySnapshot.forEach(async (bubbleMember) => {
+        counter++;
+        const userRef = bubbleMember.data().memberID;
+
+        const bubbles = await useFetchBubbleMembers(bubbleRef, userRef);
+        console.log("List of bubbles with memberID: " + bubbles);
+
+        bubbles.map(async (item) => {
+          console.log("BUBBEID " + item.bubbleID);
+
+          if (item.computerGenerated) {
+            const bubbleCreator = await getDoc(item.creatorID);
+            console.log("bubbleCreator " + bubbleCreator.data().userID);
+            if (bubbleCreator.data().userID == auth.currentUser.uid) {
+              console.log("BUBBEstatus " + item.computerGenerated);
+
+              const standAloneBubble = doc(database, "bubbles", item.bubbleID);
+              const data = await updateDoc(standAloneBubble, {
+                statusID: defaultStatus,
+                lastChanged: currentTime,
+              })
+                .then(() => {
+                  console.log("Bubble of bubblemember status reset!");
+                })
+                .catch((error) => console.error(error));
+            }
+          }
+        });
+      });
+    });
+  };
 
   useLayoutEffect(() => {
     const initialUpdate = async () => {
@@ -49,41 +108,6 @@ const StatusForSpecificAudience = ({ navigation }) => {
 
     initialUpdate();
   }, []);
-
-  // useLayoutEffect(() => {
-  //   const userRef = doc(database, "users", auth.currentUser.uid);
-  //   const q = query(
-  //     collection(database, "bubbles"),
-  //     where("creatorID", "==", userRef)
-  //   );
-
-  //   const unsubscribe = onSnapshot(q, (querySnapshot) => {
-  //     const bubblesArray = [];
-  //     let counter = 0;
-  //     if (querySnapshot.empty) return;
-
-  //     querySnapshot.forEach(async (doc) => {
-  //       counter++;
-
-  //       // const bubbleSnap = await getDoc(doc.data().bubbleID);
-  //       const dataSnap = await getDoc(doc.data().statusID);
-  //       console.log("dataSnap.data()");
-  //       console.log(dataSnap.data());
-
-  //       bubblesArray.push({
-  //         bubbleID: doc.data().bubbleID,
-  //         title: doc.data().title,
-  //         indicator: doc.data().statusID,
-  //       });
-
-  //       if (querySnapshot.size == counter) setBubbles(bubblesArray);
-
-  //       console.log("BUBBLES: " + bubblesArray);
-  //     });
-  //   });
-
-  //   return () => unsubscribe();
-  // }, []);
 
   useLayoutEffect(() => {
     const userRef = doc(database, "users", auth.currentUser.uid);
@@ -104,6 +128,7 @@ const StatusForSpecificAudience = ({ navigation }) => {
           bubbleID: doc.data().bubbleID,
           title: doc.data().title,
           indicator: doc.data().statusID,
+          computerGenerated: doc.data().computerGenerated,
         });
 
         if (querySnapshot.size == counter) setBubbles(bubblesArray);
@@ -114,63 +139,90 @@ const StatusForSpecificAudience = ({ navigation }) => {
   }, []);
 
   return (
-    <SafeAreaView style={statusForSpecificAudienceStyles.container}>
-      <Image source={bgImg} style={messagesStyles.bgImg} />
-      <Header title="Status for Specific Audience" navigation={navigation} />
+    <>
+      <ResetPopup
+        isResetModalVisible={isResetModalVisible}
+        setResetModalVisible={setResetModalVisible}
+        resetStatusFunction={resetStatusFunction}
+        title={selectedBubbleTitle}
+      />
+      <SafeAreaView style={statusForSpecificAudienceStyles.container}>
+        <Image source={bgImg} style={messagesStyles.bgImg} />
+        <Header title="Status for Specific Audience" navigation={navigation} />
 
-      <View style={statusForSpecificAudienceStyles.bgContainer}>
-        <View style={statusForSpecificAudienceStyles.descriptionContainer}>
-          <View
-            style={statusForSpecificAudienceStyles.descriptionTextContainer}
-          >
-            <Text style={statusForSpecificAudienceStyles.descriptionTextHeader}>
-              Set Status to Specific Audience
-            </Text>
-            <Text
-              style={statusForSpecificAudienceStyles.descriptionTextContent}
+        <View style={statusForSpecificAudienceStyles.bgContainer}>
+          <View style={statusForSpecificAudienceStyles.descriptionContainer}>
+            <View
+              style={statusForSpecificAudienceStyles.descriptionTextContainer}
             >
-              You can create an audience to set them to a specific status that
-              is different from your default status. If a duration is set, your
-              status for that audience will return to your default status after
-              the set duration.
-            </Text>
+              <Text
+                style={statusForSpecificAudienceStyles.descriptionTextHeader}
+              >
+                Set Status to Specific Audience
+              </Text>
+              <Text
+                style={statusForSpecificAudienceStyles.descriptionTextContent}
+              >
+                You can create an audience to set them to a specific status that
+                is different from your default status. If a duration is set,
+                your status for that audience will return to your default status
+                after the set duration.
+              </Text>
+            </View>
           </View>
-        </View>
 
-        <View style={statusForSpecificAudienceStyles.audienceContainer}>
-          <View style={statusForSpecificAudienceStyles.audienceContainerHeader}>
-            <Text
-              style={
-                statusForSpecificAudienceStyles.audienceContainerTextHeader
+          <View style={statusForSpecificAudienceStyles.audienceContainer}>
+            <View
+              style={statusForSpecificAudienceStyles.audienceContainerHeader}
+            >
+              <Text
+                style={
+                  statusForSpecificAudienceStyles.audienceContainerTextHeader
+                }
+              >
+                Select Audience
+              </Text>
+
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate("CreateAudience");
+                }}
+              >
+                <View>
+                  <Text>+ ADD</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+
+            {bubbles.map((item) => {
+              if (
+                JSON.stringify(item.indicator) != JSON.stringify(defaultStatus)
+              ) {
+                let routeName = "";
+                console.log("computerGenerated: " + item.computerGenerated);
+                item.computerGenerated
+                  ? (routeName = "ChangeStatusIndiv")
+                  : (routeName = "EditBubble");
+
+                return (
+                  <AudienceBox
+                    key={item.bubbleID}
+                    audienceIndicator={item.indicator}
+                    title={item.title}
+                    bubbleID={item.bubbleID}
+                    resetModal={setResetModalVisible}
+                    bubbleSelectedFunction={setSelectedBubble}
+                    bubbleTitleSelectedFunction={setSelectedBubbleTitle}
+                    navigation={navigation}
+                    route={routeName}
+                  />
+                );
               }
-            >
-              Select Audience
-            </Text>
-
-            <TouchableOpacity>
-              <View>
-                <Text>+ ADD</Text>
-              </View>
-            </TouchableOpacity>
+            })}
           </View>
-
-          {bubbles.map((item) => {
-            if (
-              JSON.stringify(item.indicator) != JSON.stringify(defaultStatus)
-            ) {
-              return (
-                <AudienceBox
-                  key={item.title}
-                  audienceIndicator={item.indicator}
-                  title={item.title}
-                  bubbleID={item.bubbleID}
-                />
-              );
-            }
-          })}
         </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </>
   );
 };
 
